919a52ec9513a4753339b2aef400ebb7e5c4ba41
MicPy <michael.dugang@gmail.com>
2016-04-06 22:31:07 +0800

    This is first day of learning python the hard way.ex1 to ex4.

---
+++ ex2.py
@@ -0,0 +1,27 @@
+# This is a comment.
+# Any thing you want to change you should give it a comment.
+
+print " I could have code like this." #this is ignored.
+print "This wil run"
+
+print "I will now count my chickens:"
+print "Hens", 25+30/6
+print "Roosters", 100-25*3%4
+
+print "Now I will count the eggs:"
+print 3+2+2+10
+print "Is it true that 3+2<5-7?"
+print 3+2<5-7
+
+print "Is it greater?",5>-2
+print "Is it greater or equal?",5>=-2
+print "Is it less or equal?", 5<=-2
+
+# about floating porint number
+print "Roosters:", 100.0 - 25/3.0
+
+# Ex4
+cars = 100
+cars_not_driver = cars-43
+print "We have",cars_not_driver, "cars"
+print "cars_not_driver"
